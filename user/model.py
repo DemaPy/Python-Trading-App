caf2ea9cb57c6d9dd7212daaf8c907e8cd8fe2ff
DemaPy <benderdema@gmail.com>
2025-09-14 14:35:55 +0200

    feat: create User db model

---
+++ user/model.py
@@ -0,0 +1,16 @@
+from database import Base
+from sqlalchemy import Column, Integer, String, TIMESTAMP, Boolean, text
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    username = Column(String, nullable=False, unique=True)
+    first_name = Column(String, nullable=False)
+    last_name = Column(Boolean, server_default='TRUE')
+    email = Column(String, nullable=False, unique=True)
+    is_active = Column(Boolean, server_default='TRUE', nullable=False)
+    is_superuser = Column(Boolean, server_default='FALSE', nullable=False)
+    hashed_password = Column(String, nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True), server_default=text('now()'))
